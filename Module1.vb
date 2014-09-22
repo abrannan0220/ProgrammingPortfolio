3d6017639baa64ddf44d6febab528946de976164
abrannan0220 <andrewbrannan@live.com>
2014-09-22 06:27:07 -0500

    Reservation App
    A small application to charge for reservations

---
+++ Module1.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Declare Variables
+        Dim compName As String
+        Dim numGuests As Integer
+        Dim resDate As String
+        Dim totalCost As Integer
+        Dim resCharge As Integer = 5
+
+
+        Console.WriteLine("Welcome to my Reservation Program!")
+
+        'Get Input
+
+        Console.Write("What is the name of your Company? ")
+        compName = Console.ReadLine()
+
+        Console.Write("How many Guests will be attending? ")
+        numGuests = Console.ReadLine()
+
+        Console.Write("What is the desired Date for your Reservation? ")
+        resDate = Console.ReadLine()
+
+        'Process
+
+        totalCost = numGuests * resCharge
+
+        'Output
+
+        Console.WriteLine("For the company " & compName & ".")
+        Console.WriteLine("A reservation has been made on " & resDate & " for a total of $" & totalCost & "!!!")
+        Console.ReadLine()
+    End Sub
+
+End Module
