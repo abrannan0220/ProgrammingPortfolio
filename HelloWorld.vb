1ee807c0f40cb739eada3dd9b7d4355bebb930df
abrannan0220 <andrewbrannan@live.com>
2014-09-22 06:27:54 -0500

    Hello World
    Hello World

---
+++ HelloWorld.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello World!")
+
+        Console.Write("Press ENTER to Continue...")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
